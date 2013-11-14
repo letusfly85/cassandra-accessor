29d19a5c9bde791e92ba87aac16c2b1795c1578a
letusfly85 <letusfly95@gmail.com>
2013-11-15 00:53:10 +0900

    add cassandra manager and its method 'client'

--- src/main/scala/com/jellyfish85/cassandra/accessor/manager/CassandraManager.scala
+++ src/main/scala/com/jellyfish85/cassandra/accessor/manager/CassandraManager.scala
@@ -1,5 +1,13 @@
 package com.jellyfish85.cassandra.accessor.manager
 
+import org.apache.cassandra.thrift.Cassandra
+import org.apache.thrift.protocol.TBinaryProtocol
+import org.apache.thrift.protocol.TProtocol
+import org.apache.thrift.transport.TSocket
+import org.apache.thrift.transport.TTransport
+import org.apache.cassandra.exceptions.CassandraException
+import org.apache.thrift.TException
+
 /**
  * == CassandraManager ==
  *
@@ -7,5 +15,34 @@ package com.jellyfish85.cassandra.accessor.manager
  *
  */
 class CassandraManager {
+
+  /**
+   * == client ==
+   *
+   * @return
+   */
+  def client: Cassandra.Client = {
+    var client:   Cassandra.Client = null
+
+    try {
+      val port:     TTransport        = new TSocket("localhost", 9160)
+      val protocol: TProtocol         = new TBinaryProtocol(port)
+      client = new Cassandra.Client(protocol)
+      port.open()
+
+    } catch {
+      case e0: CassandraException =>
+        e0.printStackTrace()
+
+      case e1: TException =>
+        e1.printStackTrace()
+
+      case ex: Exception =>
+        ex.printStackTrace()
+
+    }
+
+    client
+  }
 
 }
